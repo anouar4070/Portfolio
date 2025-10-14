12173d83057b919fc34fbdb72c49dc3a6843c693
anouar garbaa <g.anouar@yahoo.com>
2025-10-14 15:23:20 +0200

    adding User Module

---
+++ app/src/user/user.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { User } from './user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  providers: [UserService],
+  controllers: [UserController],
+  imports: [TypeOrmModule.forFeature([User])],
+})
+export class UserModule {}
