e60a2237be4f393215ef2d2f1e75bf3896a07534
anouar garbaa <g.anouar@yahoo.com>
2025-10-20 23:01:59 +0200

    List Article (Pagination)

--- app/src/article/article.controller.ts
+++ app/src/article/article.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
   Post,
@@ -12,6 +13,10 @@ import { ArticleRequest } from './dto/article-request.dto';
 import { AuthGuard } from '../auth/auth.guard';
 import { CurrentUser } from '../auth/current-user.decorator';
 import { User } from '../user/user.entity';
+import { Pagination } from '../shared';
+import { Page } from '../shared/pagination/pagination.decorator';
+//import { Page } from '../shared/pagination/pagination.decorator';
+//import { Pagination } from '../shared';
 
 @Controller('articles')
 export class ArticleController {
@@ -43,5 +48,10 @@ export class ArticleController {
     @Param('id') id: number,
   ): Promise<{ published: boolean }> {
     return this.articleService.publish(id, user);
+  }
+
+  @Get()
+  async getArticles(@Page() page: Pagination) {
+    return this.articleService.getArticles(page);
   }
 }
