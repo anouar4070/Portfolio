22e4ad9da249452f137b875aaec6bc3d43db5da2
anouar garbaa <g.anouar@yahoo.com>
2025-10-21 18:01:45 +0200

    Adding Article DTO

--- app/src/article/article.service.ts
+++ app/src/article/article.service.ts
@@ -9,6 +9,7 @@ import { Article } from './article.entity';
 import { Repository } from 'typeorm';
 import { generateUniqueValue, Pagination } from '../shared';
 import { User } from '../user/user.entity';
+import { ShortArticle } from './dto/article-response.dto';
 
 @Injectable()
 export class ArticleService {
@@ -79,9 +80,10 @@ export class ArticleService {
       skip,
       take: size,
       order: this.getOrder(sort, direction),
+      relations: ['user'],
     });
     return {
-      content,
+      content: content.map((article) => new ShortArticle(article)),
       page,
       size,
       total: Math.ceil(count / size),
